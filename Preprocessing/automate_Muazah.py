76022fd523f18409a7860cb8d0fff547862249d5
muazahalwyh <muazahmuazah2001@gmail.com>
2025-05-30 06:45:35 +0700

    push automate.py yml

--- Preprocessing/automate_Muazah.py
+++ Preprocessing/automate_Muazah.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE # type: ignore
+import os
+from datetime import datetime
 
 def preprocess_data(df, target_col):
     df_copy = df.copy()
@@ -30,9 +32,20 @@ def preprocess_data(df, target_col):
     smote = SMOTE(random_state=42)
     X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
 
-    X_train_resampled.to_csv("Preprocessing/Dataset/X_train_resampled.csv", index=False)
-    X_test.to_csv("Preprocessing/Dataset/X_test.csv", index=False)
-    y_train_resampled.to_csv("Preprocessing/Dataset/y_train_resampled.csv", index=False)
-    y_test.to_csv("Preprocessing/Dataset/y_test.csv", index=False)
+    # Buat folder output
+    os.makedirs("Preprocessing/Dataset", exist_ok=True)
     
+    # Tambahkan timestamp agar selalu terdeteksi perubahan
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    # Simpan file
+    X_train_resampled.to_csv(f"Preprocessing/Dataset/X_train_resampled_{timestamp}.csv", index=False)
+    X_test.to_csv(f"Preprocessing/Dataset/X_test_{timestamp}.csv", index=False)
+    y_train_resampled.to_csv(f"Preprocessing/Dataset/y_train_resampled_{timestamp}.csv", index=False)
+    y_test.to_csv(f"Preprocessing/Dataset/y_test_{timestamp}.csv", index=False)
+
     return X_train_resampled, X_test, y_train_resampled, y_test
+
+if __name__ == "__main__":
+    df = pd.read_csv("Dataset/data_bersih_preprocessing.csv")
+    preprocess_data(df, target_col="Churn Label") 
